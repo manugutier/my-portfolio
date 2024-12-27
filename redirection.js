a40f97db5f29f99aee8fa9619744067e29401af2
Manuel Gutiérrez Rivas <89094000+manulogics@users.noreply.github.com>
2024-12-26 20:14:56 -0600

    Update redirection.js

--- redirection.js
+++ redirection.js
@@ -1,6 +1,6 @@
 const pathMap = {
   "/about": "https://manulogics.notion.site/https://manulogics.notion.site/About-Me-9a69783a6a104b06a5726f03b8449659&path=about",
-  "manulogics.com/projects": "https://manulogics.notion.site/3e839863263948658a95588133b6a431?v=295eabf6bb794a93abba0e8ce8194c93%path=projects",
+  "/projects": "https://manulogics.notion.site/3e839863263948658a95588133b6a431?v=295eabf6bb794a93abba0e8ce8194c93%path=projects",
   "/projects/fay-keeper": "https://manulogics.notion.site/Fay-Keeper-400d1a9e709c4850b6caa1aabd47c13d&path=fay-keeper",
 };
 
